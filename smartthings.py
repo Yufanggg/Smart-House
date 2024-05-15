796ad48b3a72d9ee678815d39e1e7fecf2e75e17
Wessel Porschen <wesselporschen@gmail.com>
2024-05-15 17:36:16 +0200

    Added multi-file inserting to smartthings.py

--- smartthings.py
+++ smartthings.py
@@ -1,16 +1,13 @@
 import sys
 import gzip
 import pandas as pd
-from os import listdir
-from os.path import isfile, join
+import glob
 from sqlalchemy.dialects.sqlite import insert
 from sqlalchemy import create_engine, inspect, Table, Column, Integer, Float, MetaData, String
 from home_messages_db import HomeMessagesDB
 
-# TO DO: process multiple .tsv.gz files in one line
-
 TABLE_NAME = 'smartthings'
-TABLE_COLUMNS = ['loc', 'level', 'name', 'epoch', 'capability', 'attribute', 'value', 'unit']
+TABLE_COLUMNS = ['loc', 'level', 'name', 'time', 'capability', 'attribute', 'value', 'unit']
 TABLE_TYPES = [String, String, String, Integer, String, String, String, String]
 
 def main(db_url: str, filepath: str):
@@ -22,12 +19,16 @@ def main(db_url: str, filepath: str):
     df.drop_duplicates(inplace=True)
     df.dropna(inplace=True, how='all')
 
+    df.rename(columns={'epoch': 'time'}, inplace=True)
+
     # insert df into TABLE
-    print(f"Inserting {sys.argv[3]} into {sys.argv[2]}...")
+    print(f"Inserting {filepath} into {db_url}...")
     MyDB.insert_df(df=df, table=TABLE_NAME, dtype = dict(zip(TABLE_COLUMNS, TABLE_TYPES)), if_exists='append')
 
 
 if __name__ == '__main__':
+    sys.argv = [str(arg) for arg in sys.argv]
+
     if len(sys.argv) < 1:
         print('no options, please view -h')
         exit()
@@ -39,7 +40,8 @@ Options:
     -h, --help    Show this help message
     -d DBURL insert into the project database (DBURL is a SQLAlchemy database URL)
        example:
-       smartthings.py -d sqlite:///myhome.db smartthings.202210.tsv.gz
+       smartthings.py -d 'sqlite:///myhome.db' 'data/smartthings/smartthings.202210.tsv.gz'
+       smartthings.py -d 'sqlite:///myhome.db' 'data/smartthings/smartthings.*'
             """)
         
     if sys.argv[1] == '-d':
@@ -53,4 +55,6 @@ Options:
         MyDB.create_table(name=TABLE_NAME, columns=TABLE_COLUMNS, columnTypes=TABLE_TYPES )
 
         # main function (loading / cleaning / inserting of data into db)
-        main(sys.argv[2], sys.argv[3])
+        for file in glob.glob(sys.argv[3]):
+            main(sys.argv[2], file)
+        print("Done.")
