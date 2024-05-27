65c48d3b948f2c2dd7588d14ad06b72d6adee286
Agnes Czech <nerrieyt@gmail.com>
2024-05-27 04:38:03 +0200

    changed openweathermap.py to work with openmeteo data

--- openweathermap.py
+++ openweathermap.py
@@ -1,109 +1,72 @@
-import requests #these 3 are for the API to fetch, and the json to read the data
-from urllib.parse import urlparse
-import json
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
 import pandas as pd
 from datetime import datetime, date, time
-#import validators - this doesn't work for me for some reason, but it seems like the code (the errors) works without it, correct me if I'm wrong
-from home_messages_db import HomeMessagesDB #the subsequent 3 imports are for the homemessages class to work
-from sqlalchemy.dialects.sqlite import insert
-from sqlalchemy import create_engine, MetaData, text, Table, Column, Float, Integer, String, PrimaryKeyConstraint, select, inspect
-from sqlite3 import Error
-import sys #this is for the help command to work - IF one is needed here (I think only the other 3 tools have a help command requirement, tell me if I understood that correctly pls)
+#from home_messages_db import HomeMessagesDB #the subsequent 3 imports are for the homemessages class to work
+#from sqlalchemy.dialects.sqlite import insert
+#from sqlalchemy import create_engine, MetaData, text, Table, Column, Float, Integer, String, PrimaryKeyConstraint, select, inspect
+#from sqlite3 import Error
+#import sys #this is for the help command to work - IF one is needed here (I think only the other 3 tools have a help command requirement, tell me if I understood that correctly pls)
+
+#requirements: pip install openmeteo-requests & pip install requests-cache retry-requests numpy pandas
 
 
+cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
 
-#MAIN AND KINDA ONLY ISSUE: THE API FOR FETCHING PAST DATA DOESNT WORK
-#It is the url given on the website, my account is "student" so I should have access to historical data, the api key is correct, yet it doesn't work
+start = input("Provide Starting Date of Measurement (in YYYY-MM-DD format): ")
+end = input("Provide Ending Date of Measurement (in YYYY-MM-DD format): ")
 
+dt_start = datetime.strptime(start, '%Y-%m-%d').date()
+dt_end = datetime.strptime(end, '%Y-%m-%d').date()
 
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://archive-api.open-meteo.com/v1/archive" #https://archive-api.open-meteo.com/v1/era5?latitude=52.19&longitude=4.44&timeformat=unixtime&start_date=2022-01-01&end_date=2022-02-01&hourly=temperature_2m,relativehumidity_2m,rain,snowfall,windspeed_10m,winddirection_10m,soil_temperature_0_to_7cm
+params = {
+	"latitude": 52.196835,
+	"longitude": 4.4833946,
+	"start_date": dt_start,
+	"end_date": dt_end,
+    "hourly": ["temperature_2m", "relative_humidity_2m", "rain", "snowfall", "wind_speed_10m", "wind_direction_10m", "soil_temperature_0_to_7cm"]
+}
+responses = openmeteo.weather_api(url, params=params)
 
-#def get_weather(city, year, month, day, start_hour, end_hour)
-    #Put the entire weather-fetching here, replace the city input with the city being a function argument once the Past data fetching works, etc.
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s") #offset seconds is also 0 in teacher's link
 
-#def make_table(weather_df) - this will be done in one go in get_weather, but I put that here for now in case I am forgetting about something
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+#hourly_time = hourly.Variables(0).ValuesAsNumpy()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_relativehumidity_2m = hourly.Variables(1).ValuesAsNumpy()
+hourly_rain = hourly.Variables(2).ValuesAsNumpy()
+hourly_snowfall = hourly.Variables(3).ValuesAsNumpy()
+hourly_windspeed_10m = hourly.Variables(4).ValuesAsNumpy()
+hourly_winddirection_10m = hourly.Variables(5).ValuesAsNumpy()
+hourly_soil_temperature_0_to_7cm = hourly.Variables(6).ValuesAsNumpy()
 
-api_key = '91f1f5e2fd5fb820b2c2e8645c5d99b5'
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True), #check if it has to be changed to gmt somehow, since the data is the same as for gmt in the teacher's link
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
 
-answer = input("Do you wish to access current or past data? Answer either Current or Past: ") #not needed if we are to deal only with historic data, I put it here and included the current option because                                                                                          #it was the only option that worked at first
-if answer=="Current": #current data
-    while True:
-        city = input("Enter City Name: ")
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric' 
-        r = requests.get(url)
-        if r.status_code == 404:
-            print("Incorrect city name")
-        else:
-            break
-    
-    data = r.json()
-    lat = data['coord']['lat'] 
-    lon = data['coord']['lon']
+#hourly_data["unixtime"] = hourly_time
+hourly_data["temperature_2m_°C"] = hourly_temperature_2m
+hourly_data["relativehumidity_2m_%"] = hourly_relativehumidity_2m
+hourly_data["rain_mm"] = hourly_rain
+hourly_data["snowfall_cm"] = hourly_snowfall
+hourly_data["windspeed_10m_km/h"] = hourly_windspeed_10m
+hourly_data["winddirection_10m_°"] = hourly_winddirection_10m
+hourly_data["soil_temperature_0_to_7cm_°C"] = hourly_soil_temperature_0_to_7cm
 
-    city = city[0].upper() + city[1:] #What does this do?
-    time_now = datetime.now()
-    start = end = datetime.timestamp(time_now) #converts the current time to Unix Time
-
-elif answer=="Past":
-    while True:
-        city = input("Enter City Name: ")
-        url_initial = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric' 
-        r = requests.get(url_initial)
-        if r.status_code == 404:
-            print("Incorrect city name")
-        else:
-            break
-    
-    city = city[0].upper() + city[1:]
-    response = requests.get(url_initial)
-    data = response.json()
-
-    print(response)
-
-    lat = data['coord']['lat'] 
-    lon = data['coord']['lon']
-
-    year = int(input("Enter Year of Measurement: "))
-    month = int(input("Enter Month (as a number) of Measurement: "))
-    day = int(input("Enter Day of Measurement: "))
-    start_hour = int(input("Enter Starting Hour of Measurement: "))
-    end_hour = int(input("Enter Final Hour of Measurement: "))
-
-    dt_start = datetime.combine(date(year, month, day), time(start_hour))
-    dt_end = datetime.combine(date(year, month, day), time(end_hour))
-
-    start = datetime.timestamp(dt_start)
-    end = datetime.timestamp(dt_end)
-
-    url = f'https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=hour&start={start}&end={end}&units=metric&appid={api_key}' #historic data
-    #url = f'https://history.openweathermap.org/data/2.5/history/city?lat=52.2400&lon=4.4500&type=hour&start=1715292000&end=1715378400&appid=91f1f5e2fd5fb820b2c2e8645c5d99b5&units=metric'# - example: Noordwijk data from the entire day of 10th May
-
-else:
-    raise ValueError("Incorrect input")
-
-
-response = requests.get(url)
-data = response.json()
-
-temp = data['main']['temp'] #if &units=metric doesnt work and the temp is in Kelvin, subtract 273.15 to convert it to Celsius
-felt_temp = data['main']['feels_like']
-wind = data['wind']['speed'] * 3.6 #converting the wind speed unit from m/s to km/h
-press = data['main']['pressure']
-humid = data['main']['humidity']
-desc = data['weather'][0]['description'] #the description is tbh probably not needed, but I guess it also doesn't hurt to keep it
-
-weather_data = [[lat, lon, start, end, city, temp, 'Celsius', 'temperature'],
-            [lat, lon, start, end, city, felt_temp, 'Celsius', 'felt temperature'],
-            [lat, lon, start, end, city, wind, 'km/h', 'wind speed'],
-            [lat, lon, start, end, city, press, 'Pa', 'pressure'],
-            [lat, lon, start, end, city, humid, '%', 'humidity'],
-            [lat, lon, start, end, city, desc, 'description', 'description']]
-weather_df = pd.DataFrame(weather_data, columns=['Latitude', 'Longitude', 'Start Time', 'End Time', 'City', 'Value', 'Unit', 'Measure'])
-
-print(weather_df) #This is in df format but no longer shows the exact Unix Time in contrast to just using print() to show data
-
-#I guess this is not needed if we decide not to store the weather data in our database but I decided to put this prototype here for now anyway:
-#home_db = HomeMessagesDB('sqlite:///db/pythondqlite.db')
-#keys = {'Latitude': Float(), 'Longitude': Float(), 'Start Time': Float(), 'End Time': Float(), 'City': String()}
-#home_db.insert_df(df = weather_df, table = 'weather', dtype = keys, if_exists = 'replace') #Is this only gonna replace if the data are identical, or the entire table as long as the key column names are the same?
-
-
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+print(hourly_dataframe)
