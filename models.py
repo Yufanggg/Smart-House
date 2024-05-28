63e99106438b3aab6afa3b7859fe48fea2e2cd8c
bouwehand <b.j.ouwehand@gmail.com>
2024-05-28 19:45:44 +0200

    :WIP

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Float
+from sqlalchemy import Column, Integer, Float, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -18,3 +18,15 @@ class Pe1T2(Base):
     time = Column(Integer(), primary_key=True)
     imported_t1 = Column('imported T2', Float(), nullable=False, default=0)
     exported_t1 = Column('exported T2', Float(), nullable=False, default=0)
+
+
+class SmartThingsGround(Base):
+    __tablename__ = 'smartthings_ground'
+    loc = Column('loc', String(), nullable=True)
+    level = Column('level', String(), nullable=True)
+    name = Column('name', String(), nullable=True)
+    time = Column('time', Integer(), nullable=True, primary_key=True)
+    capability = Column('capability', String(), nullable=True)
+    attribute = Column('attribute', String(), nullable=True)
+    value = Column('value', String(), nullable=True)
+    unit = Column('unit', String(), nullable=True)
